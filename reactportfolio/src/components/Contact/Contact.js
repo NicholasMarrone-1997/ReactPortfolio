abfd6aa9470942e2ddd51a8298a3d9951a647919
Nicholas Marrone <nmarrone1997@gmail.com>
2021-04-15 15:24:39 -0400

    Can click on email now and redirect to page, but page is blank

--- reactportfolio/src/components/Contact/Contact.js
+++ reactportfolio/src/components/Contact/Contact.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Nav from '../Header/Nav';
 import './Contact.css';
 
+const mailto =
+"mailto:marrone1997@gmail.com?subject=Test subject&body=Body content";
 const Contact = () => {
     return (
         <div>
@@ -13,7 +15,7 @@ const Contact = () => {
         <div className="links-me col text-center">
           <a href="https://github.com/NicholasMarrone-1997"><button>Github</button></a>
           <a href="https://www.linkedin.com/in/nmarrone1/"><button>Linkedin</button></a>
-          <a href={"mailto:" + "nmarrone1997@gmail.com"} target="__blank">
+          <a href={mailto} target="__blank" id="btn">
              Email
                 </a>
           <a href="tel:732-275-7312"><button>Phone</button></a>
