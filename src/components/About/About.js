39c155c5549ef8989fb7f21635f0ab84cdddc550
Nicholas Marrone <nmarrone1997@gmail.com>
2021-04-15 20:01:44 -0400

    Successfully deployed to heroku. Making additional changes to css on each page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,15 +5,17 @@ import './About.css';
 
 const About = () => {
     return(
-      <div className="page">
+  <div className="page">
       <Nav />
-    <div className="about-wrapper row">
-        <div className="col-md-6">
+  <div className="container">
+    <div className="row">
+        <div className="col">
           <h2 id="about">About Me</h2>
           <img id="headshot" src='/headshot.jfif' alt="Italian Trulli" />
         </div>
+      </div>
 
-        <div className="col-md-6">
+        <div className="col">
           <p className="about-me">Hello, my name is Nicholas Marrone. I am 24 years old and am originally from Atlantic
             Highlands, NJ. I graduated from the University of North Carolina at Charlotte in 2019 with a BS in Computer
             Science. I am currently attending a full-stack web development bootcamp where I hope to hone my skills as a
@@ -22,7 +24,7 @@ const About = () => {
         </div>
       </div>
       <Footer />
-      </div>
+    </div>
     )
 }
 
